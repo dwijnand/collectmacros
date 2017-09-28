35507dd99183ee440f070852e277d63b16c67234
Jon Pretty <jon.pretty@propensive.com>
2017-09-28 16:34:52 +0200

    Consider making constructors available on existing companion objects
    This uses the extension-method pattern to provide a `construct` method
    on existing `List`, `Vector` and `Array` companion objects.

--- src/main/scala/collectmacros/package.scala
+++ src/main/scala/collectmacros/package.scala
@@ -2,10 +2,6 @@ package collectmacros
 
 import scala.reflect.macros.blackbox
 
-object xList {
-  def apply[A](xs: A*): List[A] = macro xList.applyImpl[A]
-}
-
 final class xList(val c: blackbox.Context) {
   import c.universe._
 
@@ -14,10 +10,6 @@ final class xList(val c: blackbox.Context) {
 
   def applyImpl[A](xs: Tree*)(implicit A: WeakTypeTag[A]) =
     xs.foldRight(NilObject: Tree)((next, acc) => q"new $ConsType[$A]($next, $acc)")
-}
-
-object xVector {
-  def apply[A](xs: A*): Vector[A] = macro xVector.applyImpl[A]
 }
 
 final class xVector(val c: blackbox.Context) {
@@ -29,10 +21,6 @@ final class xVector(val c: blackbox.Context) {
     if (xs.isEmpty) q"$VectorObject.empty[$A]"
     else q"{ val b = $VectorObject.newBuilder[$A]; ..${xs map (x => q"b += $x")}; b.result() }"
   }
-}
-
-object xArray {
-  def apply[A](xs: A*): Array[A] = macro xArray.applyImpl[A]
 }
 
 final class xArray(val c: blackbox.Context) {
@@ -56,4 +44,20 @@ object Desugar {
 final class Desugar(val c: blackbox.Context) {
   import c.universe._
   def desugarImpl(a: Tree) = Literal(Constant(showRaw(a)))
+}
+
+object `package` {
+
+  implicit class ListExt(val companion: List.type) {
+    def construct[A](xs: A*): List[A] = macro xList.applyImpl[A]
+  }
+
+  implicit class VectorExt(val companion: Vector.type) {
+    def construct[A](xs: A*): Vector[A] = macro xVector.applyImpl[A]
+  }
+
+  implicit class ArrayExt(val companion: Array.type) {
+    def construct[A](xs: A*): Array[A] = macro xArray.applyImpl[A]
+  }
+
 }
